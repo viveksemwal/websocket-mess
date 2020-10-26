85e820c5d44dc01191430ea076138e0c7c5739ae
viveksemwal <Viveksemwal1997@gmail.com>
2020-10-26 14:32:10 +0530

    Initial commit

---
+++ src/test/java/com/vivek/websocketmess/WebsocketMessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.websocketmess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketMessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
